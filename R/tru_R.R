1e219e261aed139a4acab3863914dedafd06d4f9
naeemkh <khoshnevis.naeem@gmail.com>
2022-02-14 21:50:33 -0500

    separate sim_fns func

---
+++ R/tru_R.R
@@ -0,0 +1,7 @@
+tru_R<-function(w, sim.data){
+  design.mt = model.matrix(~cf1+cf2+cf3+cf4+cf5+cf6-1, data = sim.data)
+  mean(apply(design.mt, 1, function(x){
+    -10 - sum(c(2, 2, 3, -1,2,2)*x) -
+      w*(0.1 - 0.1*x[1] + 0.1*x[4] + 0.1*x[5] + 0.1*x[3]^2) + 0.13^2*w^3
+  }))
+}
