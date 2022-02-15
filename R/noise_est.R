0394f7b58f915d0b514f589d9efc08bf8a23f94a
naeemkh <khoshnevis.naeem@gmail.com>
2022-02-14 21:41:21 -0500

    separate GP_fns funcs

---
+++ R/noise_est.R
@@ -0,0 +1,11 @@
+# estimate sigma
+noise.est = function(param, sim.data, GPS){
+  # browser()
+  n.sample = nrow(sim.data)
+  w.all = rep(sim.data$treat, 2)
+  GPS.all = rep(GPS, 2)
+  Sigma.all = param[3]*exp(-as.matrix(dist(cbind(w.all*sqrt(param[1]), GPS.all*sqrt(param[2]))))) +
+    diag(2*n.sample)
+
+  sd(sim.data$Y - Sigma.all[1:n.sample,-(1:n.sample)]%*%chol2inv(chol(Sigma.all[-(1:n.sample),-(1:n.sample)]))%*%sim.data$Y)
+}
