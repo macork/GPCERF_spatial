1a224d2b6b3d351aadde9ed9a4ff01b049d3331e
naeemkh <khoshnevis.naeem@gmail.com>
2022-03-29 15:04:32 -0400

    Add function and test to compute weighted correlation

---
+++ R/compute_w_corr.R
@@ -0,0 +1,55 @@
+#' @title
+#' Compute Weighted Correlation
+#'
+#' @description
+#' Computes weighted correlation of the observational data based on weights
+#' achieved by Gaussian Process.
+#'
+#' @param data A data.table of observational data with the following colums:
+#'   - Column 1: Outcome (Y)
+#'   - Column 2: Exposure or treatment (w)
+#'   - Column 3~m: Confounders (C)
+#' @param weights A vector of weights for each observation data.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+#' mydata <- generate_synthetic_data(sample_size = 200)
+#' setDT(mydata)
+#' weights <- runif(nrow(mydata)
+#' compute_w_corr(mydata, weights)
+#'
+compute_w_corr <- function(data, weights){
+
+
+  if (!is.data.table(data)){
+    stop(paste0("The data should be a data.table. ",
+                "Current format: ", class(obs_data)[1]))
+  }
+
+  if (nrow(data) != length(weights)){
+    stop(paste0("Number of data samples (", nrow(data), ") and length of ",
+                "weights (", length(weights),") should be equal."))
+  }
+
+  # TODO: model.matrix will create dummy vairables for factors.
+  # Double check with Boyu.
+  conf_names <- colnames(data[,3:ncol(data)])
+  frml <- paste("~",paste(conf_names, collapse = "+"), "-1", sep = "")
+
+  exposure_data <- data[,2]
+
+  x_design = model.matrix(as.formula(frml), data = data)
+  w_mean = sum(exposure_data*weights)
+  w_sd = sqrt(sum((exposure_data - w_mean)^2*weights))
+  w_stan = (exposure_data - w_mean)/w_sd
+
+  x_mean = colMeans(x_design*weights)
+  x_cov = (t(x_design) - x_mean)%*%diag(weights)%*%t(t(x_design) - x_mean)
+  x_stan = t(t(solve(chol(x_cov)))%*%(t(x_design) - x_mean))
+  covariate_balance <- abs(c(t(x_stan)%*%diag(weights)%*%w_stan))
+
+  return(covariate_balance)
+}
---
+++ tests/testthat/test-compute_w_corr.R
@@ -0,0 +1,24 @@
+test_that("Weighted correlation works as expected.", {
+
+  set.seed(239)
+  data1 <- generate_synthetic_data(sample_size = 100)
+  setDT(data1)
+  weights1 <- runif(nrow(data1))
+  val1 <- compute_w_corr(data = data, weights = weights1)
+
+  expect_vector(val1)
+  expect_equal(length(val1), 6L)
+  expect_equal(val1[1], 0.05986759, tolerance = 0.00001)
+  expect_equal(val1[2], 0.002001404, tolerance = 0.00001)
+
+  # data should be a data.table
+  data2 <- generate_synthetic_data(sample_size = 50)
+  weights2 <- runif(nrow(data2))
+  expect_error(compute_w_corr(data = data2, weights = weights2))
+
+  # number of data.samples and weights should be the same
+  data3 <- generate_synthetic_data(sample_size = 50)
+  weights3 <- runif(nrow(data3)+20)
+  expect_error(compute_w_corr(data = data3, weights = weights3))
+
+})
