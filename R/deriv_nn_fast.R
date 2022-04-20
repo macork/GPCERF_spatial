c89f2cff6f597292f9aa29f49953859c49b6ebaa
boyuren158 <boyuren158@gmail.com>
2022-04-20 13:01:11 -0400

    Fix bugs in deriv_nn_fast.R

--- R/deriv_nn_fast.R
+++ R/deriv_nn_fast.R
@@ -9,7 +9,7 @@
 #' @param w.obs A vector of observed exposure levels of all samples.
 #' @param GPS.obs A vector of GPS for all samples at the observed levels of exposure.
 #' @param y.obs A vector of observed outcome values.
-#' @param param A vector of hyper-parameters in the GP model.
+#' @param params A vector of hyper-parameters in the GP model.
 #' @param e_gps_pred A vecotor of estimated conditional means of the exposure given covariates
 #' for all samples.
 #' @param e_gps_std A scalar of estimated conditional standard deviations of the exposure given covariates.
@@ -17,19 +17,19 @@
 #' @param expand Scaling factor to determine the total number of nearest neighbours. The total is \code{2*expand*n.neighbour}.
 #' @param block.size Number of samples included in a computation block. Mainly used to
 #' balance the speed and memory requirement. Larger \code{block.size} is faster, but requires more memory.
-#' @param kernel.fn The covariance function.
-#' @param kernel.deriv.fn The partial derivative of the covariance function.
+#' @param kernel.fn The covariance function. The input is the square of Euclidean distance.
+#' @param kernel.deriv.fn The partial derivative of the covariance function. The input is the square of Euclidean distance.
 #'
 #' @return
 #' A scalar of estimated derivative of CERF at \code{w} in nnGP.
 #' @export
 #'
 #' @examples
-deriv.nn.fast = function(w, w.obs, GPS.obs, y.obs, param, e_gps_pred, e_gps_std,
-                                 n.neighbours, expand, block.size,
-                                 kernel.fn = function(x) exp(-x^2),
-                                 kernel.deriv.fn = function(x, mu, sigma) (x-mu)/sigma^2){
-  # param[1]: alpha, param[2]: beta, param[3]: gamma
+deriv.nn.fast = function(w, w.obs, GPS.obs, y.obs, params, e_gps_pred, e_gps_std,
+                                 n.neighbour, expand, block.size,
+                                 kernel.fn = function(x) exp(-x),
+                                 kernel.deriv.fn = function(x) -exp(-x)){
+  # params[1]: alpha, params[2]: beta, params[3]: gamma
   # cov = gamma*h(alpha*w^2 + beta*GPS^2) + diag(1)
   GPS.new = dnorm(w, mean = e_gps_pred, sd = e_gps_std)
 
@@ -37,12 +37,13 @@ deriv.nn.fast = function(w, w.obs, GPS.obs, y.obs, param, e_gps_pred, e_gps_std,
   n.block = ceiling(n/block.size)
   obs.raw = cbind(w.obs, GPS.obs)
   obs.ord = obs.raw[order(obs.raw[,1]),]
+  y.obs.ord = y.obs[order(obs.raw[,1])]
   #params: length 3, first scale for w, second scale for GPS,
   #third scale for exp fn
-  if(w.new >= obs.ord[nrow(obs.ord),1]){
+  if(w >= obs.ord[nrow(obs.ord),1]){
     idx.all = seq( nrow(obs.ord) - expand*n.neighbour + 1, nrow(obs.ord), 1)
   }else{
-    idx.anchor = which.max(obs.ord[,1]>=w.new)
+    idx.anchor = which.max(obs.ord[,1]>=w)
     idx.start = max(1, idx.anchor - n.neighbour*expand)
     idx.end = min(nrow(obs.ord), idx.anchor + n.neighbour*expand)
     if(idx.end == nrow(obs.ord)){
@@ -53,20 +54,20 @@ deriv.nn.fast = function(w, w.obs, GPS.obs, y.obs, param, e_gps_pred, e_gps_std,
   }
 
   obs.use = t(t(obs.ord[idx.all,])*sqrt(params[1:2]))
-  cov.use.inv = chol2inv(chol(params[3]*exp(-as.matrix(dist(obs.use))^2) + diag(nrow(obs.use))))
   y.use = y.obs.ord[idx.all]
 
-  obs.new = t(t(cbind(w.new, GPS.new))*sqrt(params[1:2]))
+  obs.new = t(t(cbind(w, GPS.new))*sqrt(params[1:2]))
   id.all = split(1:n, ceiling(seq_along(1:n)/n.block))
-  all.weights = sapply(id.all, function(id.ind){
-    Sigma.obs = param[3]*kernel.fn(as.matrix(dist(obs.use))) + diag(nrow(obs.use))
-    Sigma.cross = param[3]*kernel.fn(spatstat.geom::crossdist(obs.new[id.ind,1], obs.new[id.ind,2],
-                                                              obs.use[,1], obs.use[,2]))*
-      param[1]*outer(w, w.obs, "-") - param[2]*outer(GPS.new[id.ind], GPS.obs, "-")*
-      GPS.new*kernel.deriv.fn(w, e_gps_pred, e_gps_std)
+  Sigma.obs = params[3]*kernel.fn(as.matrix(dist(obs.use))^2) + diag(nrow(obs.use))
+  Sigma.obs.inv = chol2inv(chol(Sigma.obs))
 
+  all.weights = sapply(id.all, function(id.ind){
+    cross.dist = spatstat.geom::crossdist(obs.new[id.ind,1], obs.new[id.ind,2],
+                                          obs.use[,1], obs.use[,2])
+    Sigma.cross = params[3]*params[1]*(-2*outer(rep(w,length(id.ind))*params[1], obs.use[,1], "-"))*
+      kernel.deriv.fn(cross.dist^2)
     #mean
-    w = Sigma.cross%*%chol2inv(Sigma.obs)
+    w = Sigma.cross%*%Sigma.obs.inv
     w[w<0] = 0
     colSums(w)
   })
