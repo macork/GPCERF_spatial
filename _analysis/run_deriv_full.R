77d8d5109b75079f8f7cb71db921979015567fc2
naeemkh <khoshnevis.naeem@gmail.com>
2022-04-21 11:00:22 -0400

    Add compute_rl_deriv_gp and tests.

--- _analysis/run_deriv_full.R
+++ _analysis/run_deriv_full.R
@@ -1,15 +1,16 @@
 
-set.seed(9615)
+set.seed(847)
 data <- generate_synthetic_data(sample_size = 200)
 GPS_m <- train_GPS(cov.mt = as.matrix(data[,-(1:2)]),
                    w.all = as.matrix(data$treat))
 
-wi <- 4.2
+wi <- 8.6
 
-weights = compute_deriv_weights_gp(w = wi,
-                                   w_obs = data$treat,
-                                   GPS_m = GPS_m,
-                                   hyperparam = c(1,1,2))
+val <- compute_rl_deriv_gp(w = wi,
+                          w_obs = data$treat,
+                          y_obs = data$Y,
+                          GPS_m = GPS_m,
+                          hyperparam = c(1,1,2))
 
 deriv.est.full = sapply(seq(2.5,17.5,0.1), function(w){
   weights = GP.deriv.weights.calc(w = w,
