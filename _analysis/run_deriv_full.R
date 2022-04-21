dbbbd118068185417cbf7121aa6ba2651ccdde9d
naeemkh <khoshnevis.naeem@gmail.com>
2022-04-21 10:36:24 -0400

    add compute_deriv_weights_gp and tests.

--- _analysis/run_deriv_full.R
+++ _analysis/run_deriv_full.R
@@ -1,12 +1,20 @@
-sim.data = GPCERF::generate_synthetic_data(sample_size = 500)
 
-e_gps <- lm(treat~.-Y, data = sim.data)
-e_gps_pred <- e_gps$fitted.values
-e_gps_std <- sd(e_gps$residuals)
-GPS = dnorm(sim.data$treat, mean = e_gps_pred, sd = e_gps_std, log = T)
+set.seed(9615)
+data <- generate_synthetic_data(sample_size = 200)
+GPS_m <- train_GPS(cov.mt = as.matrix(data[,-(1:2)]),
+                   w.all = as.matrix(data$treat))
+
+wi <- 4.2
+
+weights = compute_deriv_weights_gp(w = wi,
+                                   w_obs = data$treat,
+                                   GPS_m = GPS_m,
+                                   hyperparam = c(1,1,2))
 
 deriv.est.full = sapply(seq(2.5,17.5,0.1), function(w){
-  weights = GP.deriv.weights.calc(w = w, w.obs = sim.data$treat, GPS.obs = GPS,
+  weights = GP.deriv.weights.calc(w = w,
+                                  w_obs =
+                                  sim.data$treat, GPS.obs = GPS,
                                   param = c(1,1,2), e_gps_pred = e_gps_pred,
                                   e_gps_std = e_gps_std)
   weights%*%sim.data$Y
---
+++ tests/testthat/test-compute_deriv_weights_gp.R
@@ -0,0 +1,16 @@
+test_that("compute_deriv_weights_gp works as expected!", {
+
+  set.seed(9615)
+  data <- generate_synthetic_data(sample_size = 200)
+  GPS_m <- train_GPS(cov.mt = as.matrix(data[,-(1:2)]),
+                     w.all = as.matrix(data$treat))
+
+  wi <- 4.2
+  weights <- compute_deriv_weights_gp(w = wi,
+                                      w_obs = data$treat,
+                                      GPS_m = GPS_m,
+                                      hyperparam = c(1,1,2))
+
+  expect_equal(length(weights), nrow(data))
+  expect_equal(weights[37], -3.169395e-04, tolerance = 0.00001)
+})
