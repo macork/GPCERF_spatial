9229b8e548a0948d5b4f62d779d2753b6fcd58f4
naeemkh <khoshnevis.naeem@gmail.com>
2022-05-10 09:56:18 -0400

    improve tests, ignore zzz.R

--- tests/testthat/test-generate_synthetic_data.R
+++ tests/testthat/test-generate_synthetic_data.R
@@ -8,4 +8,23 @@ test_that("generate_synthetic_data works as expected!", {
   expect_equal(length(mydata), 8L)
   expect_equal(mydata[10,2], 9.771912, tolerance = 0.00001)
 
+
+  set.seed(256)
+  mydata <- generate_synthetic_data(sample_size = 300, outcome_sd = 10,
+                                    gps_spec= 2, cova_spec=2)
+
+  expect_equal(class(mydata), "data.frame")
+  expect_equal(nrow(mydata), 300L)
+  expect_equal(length(mydata), 8L)
+  expect_equal(mydata[15,3], 1.229907, tolerance = 0.00001)
+
+  set.seed(811)
+  mydata <- generate_synthetic_data(sample_size = 300, outcome_sd = 10,
+                                    gps_spec= 3, cova_spec=2)
+
+  expect_equal(class(mydata), "data.frame")
+  expect_equal(nrow(mydata), 300L)
+  expect_equal(length(mydata), 8L)
+  expect_equal(mydata[20,1], 3.911867, tolerance = 0.00001)
+
 })
