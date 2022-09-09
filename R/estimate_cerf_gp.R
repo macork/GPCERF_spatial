3980399a3a3c9e3494d244895b3464030c6324a8
boyuren158 <boyuren158@gmail.com>
2022-09-09 13:57:30 -0400

    Add parallel computing for different hyperparameter values

--- R/estimate_cerf_gp.R
+++ R/estimate_cerf_gp.R
@@ -110,11 +110,28 @@ estimate_cerf_gp <- function(data, w, GPS_m, params, nthread = 1,
 
   # Compute m, "confidence interval", and covariate balance for provided
   # hyperparameters. -----------------------------------------------------------
+  lfp <- get_options("logger_file_path")
 
-  tune_res <- apply(tune_params_subset, 1, function(x){
-    compute_m_sigma(hyperparam = x, data = data,
-                    w = w, GPS_m = GPS_m, nthread = nthread)
+  # make a cluster
+  cl <- parallel::makeCluster(nthread, type="PSOCK",
+                              outfile= lfp)
+
+  # export variables and functions to cluster cores
+  parallel::clusterExport(cl=cl,
+                          varlist = c("w", "data", "GPS_m",
+                                      "tune_params_subset",
+                                      "nthread", "kernel_fn",
+                                      "compute_m_sigma"),
+                          envir=environment())
+
+  tune_res <- parallel::parApply(cl, tune_params_subset, 1,
+                                 function(x){
+                                   compute_m_sigma(hyperparam = x, data = data,
+                                                   w = w, GPS_m = GPS_m, nthread = nthread, kernel_fn = kernel_fn )
   })
+
+  # terminate clusters.
+  parallel::stopCluster(cl)
 
   # Select the combination of hyperparameters that provides the lowest
   # covariate balance ----------------------------------------------------------
